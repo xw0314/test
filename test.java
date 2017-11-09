c8a689bf0de1f7f5f62d029f63c05325dcb461cd
熊伟 <sivan0314@163.com>
2017-11-09 09:11:21 +0800

    understand how stage works

--- test.java
+++ test.java
@@ -3,5 +3,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println(System.getProperty("user.dir"));
+		System.out.println(11315);
 	}
 }
